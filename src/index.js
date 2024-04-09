b6e2670e88de79104a0decd04419556716a3b013
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2024-04-09 09:36:05 +0530

    feat: steps 5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
+// const parseQuery = require('./queryParser');
+// const readCSV = require('./csvReader');
+
+// async function executeSELECTQuery(query) {
+//     const { fields, table } = parseQuery(query);
+//     const data = await readCSV(`${table}.csv`);
+    
+//     // Filter the fields based on the query
+//     return data.map(row => {
+//         const filteredRow = {};
+//         fields.forEach(field => {
+//             filteredRow[field] = row[field];
+//         });
+//         return filteredRow;
+//     });
+// }
+
+// module.exports = executeSELECTQuery;
 const parseQuery = require('./queryParser');
 const readCSV = require('./csvReader');
 
 async function executeSELECTQuery(query) {
-    const { fields, table } = parseQuery(query);
+    const { fields, table, whereClause } = parseQuery(query);
     const data = await readCSV(`${table}.csv`);
     
-    // Filter the fields based on the query
-    return data.map(row => {
-        const filteredRow = {};
+    // Filtering based on WHERE clause
+    const filteredData = whereClause
+        ? data.filter(row => {
+            const [field, value] = whereClause.split('=').map(s => s.trim());
+            return row[field] === value;
+        })
+        : data;
+
+    // Selecting the specified fields
+    return filteredData.map(row => {
+        const selectedRow = {};
         fields.forEach(field => {
-            filteredRow[field] = row[field];
+            selectedRow[field] = row[field];
         });
-        return filteredRow;
+        return selectedRow;
     });
 }
 
